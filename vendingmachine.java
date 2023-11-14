a5647146973376c44aecc60dbe99785104b7fd4d
shuto kikai <yysskikai04@icloud.com>
2023-11-15 07:20:39 +0900

    init

--- vendingmachine.java
+++ vendingmachine.java
@@ -1,31 +1,37 @@
 class Drink {
-    private int price;
-    private int stock;
+  private int price;
+  private int stock;
 
-    public Drink(int price, int stock) {
-        this.price = price;
-        this.stock = stock;
-    }
+  public Drink(int price, int stock) {
+      this.price = price;
+      this.stock = stock;
+  }
 
 }
 
 class Money {
-  int bill_type[] = {10,50,100,500,1000}; 
+
+  public static void bill_type() {
+    int bill_type[] = {10,50,100,500,1000};
+  } 
 }
 
-class vendingMachine {
+class vendingMachine extends Money extends Drink{
+  public void start() {
+    System.out.println("自動販売機へようこそ");
+  }
 
 }
 
 class startVendingMachine {
-    public static void main() {
-        Drink drink = new  Drink();
-        drink.drinkMenu();
-        HashMap<String, Drink> drinkMenu = new HashMap<>();
-        drinkMenu.put("コーラ", new Drink(160, 5));
-        drinkMenu.put("水", new Drink(100, 5));
-        drinkMenu.put("モンスターエナジー", new Drink(210, 5));
-    }
+  public static void main() {
+    Drink drink = new  Drink();
+    drink.Drink();
+    HashMap<String, Drink> drinkMenu = new HashMap<>();
+    drinkMenu.put("コーラ", new Drink(160, 5));
+    drinkMenu.put("水", new Drink(100, 5));
+    drinkMenu.put("モンスターエナジー", new Drink(210, 5));
+  }
 
     public static void main(int[] args) {
         Money money = new Money();
