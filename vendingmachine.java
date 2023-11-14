cf0d42ac08e0ae97e460d64e86cd08e25419667f
shuto kikai <yysskikai04@icloud.com>
2023-11-14 18:10:27 +0900

    startVendingMachineのインスタンスを記述した

--- vendingmachine.java
+++ vendingmachine.java
@@ -11,5 +11,20 @@ class vendingMachine {
 }
 
 class startVendingMachine {
+    public static void main(String[] args) {
+        Drink drink = new  Drink();
+        drink.drinks();
+    }
 
+    public static void main(String[] args) {
+        Money money = new Money();
+        money.bill_type();
+    }
+
+    public static void main(String[] args) {
+        vendingMachine vm = new vendingMachine();
+        vm.start();
+    }
 }
+
+
