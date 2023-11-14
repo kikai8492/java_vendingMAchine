743ea3c374a2a88dcf4403bdfd1faefec5e06a53
shuto kikai <yysskikai04@icloud.com>
2023-11-14 18:48:47 +0900

    drinkをハッシュ

--- vendingmachine.java
+++ vendingmachine.java
@@ -1,4 +1,11 @@
 class Drink {
+    private int price;
+    private int stock;
+
+    public Drink(int price, int stock) {
+        this.price = price;
+        this.stock = stock;
+    }
 
 }
 
@@ -13,7 +20,11 @@ class vendingMachine {
 class startVendingMachine {
     public static void main(String[] args) {
         Drink drink = new  Drink();
-        drink.drinks();
+        drink.drinkMenu();
+        HashMap<String, Drink> drinkMenu = new HashMap<>();
+        drinkMenu.put("コーラ", new Drink(160, 5));
+        drinkMenu.put("水", new Drink(100, 5));
+        drinkMenu.put("モンスターエナジー", new Drink(210, 5));
     }
 
     public static void main(String[] args) {
