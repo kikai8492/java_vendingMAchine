b6dbabfdf31aa0faaa06f6510fb499e1534a9eed
shuto kikai <yysskikai04@icloud.com>
2023-11-17 16:34:39 +0900

    メソッドの修正

--- vendingmachine.java
+++ vendingmachine.java
@@ -1,34 +1,39 @@
 import java.util.HashMap;
-class vendingMachine {
-  private int totalMoney; //インスタンス変数にした
-  public void initialize(int totalMoney) {
-    this.totalMoney = 0; 
-  }
-  public static void drinks() {
-    HashMap<String, Integer> hmap = new HashMap<String, Integer>();
-    hmap.put("コーラ", 160);
-    hmap.put("水", 100);
-    hmap.put("モンスター", 210);
-  }
 
-  public static void money() {
-    
-  }
+class VendingMachine {
+    private int slotMoney;
+    private int salesMoney;
+    private int[] billType;
+    private HashMap<String, Integer> drinks;
 
-  public static void system() {
-    
-  }
+    public void initialize() {
+        this.slotMoney = 0;
+        this.salesMoney = 0;
+        this.drinks = new HashMap<>();
+    }
+
+    public void drinks() {
+        drinks.put("コーラ", 160);
+        drinks.put("水", 100);
+        drinks.put("モンスター", 210);
+    }
+
+    public void money() {
+        this.billType = new int[] {10, 50, 100, 500, 1000};
+    }
+
+    public void system() {
+        System.out.println("自動販売機へようこそ" );
+        
+    }
 }
 
-class startVendingMachine {
-  public static void main(String[] args) {
-    vendingMachine vm = new vendingMachine();
-    vm.drinks();
-    vm.money();
-    vm.system();
-  }
+class StartVendingMachine {
+    public static void main(String[] args) {
+        VendingMachine vm = new VendingMachine();
+        
+        vm.drinks();
+        vm.money();
+        vm.system();
+    }
 }
-
-
-
-
